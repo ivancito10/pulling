a5839ff69516fefef92f5167d98b2cf4b4579539
Ivan50000 <84816162+Ivan50000@users.noreply.github.com>
2021-05-26 20:51:12 -0400

    Update pull.php
    deje otro ejemplo mas completo para hacer un botones

--- pull.php
+++ pull.php
@@ -1,3 +1,10 @@
  <div class="container mt-5">
         <button class="btn btn-primary" data-toggle="tooltip" data-placement="top" title="Tooltip en la parte de arriba">Botón Tooltip Arriba</button>
+  
+        <button class="btn btn-warning" data-toggle="tooltip" data-placement="bottom" title="Tooltip en la parte de abajo">Botón Tooltip Abajo</button>
+
+        <button class="btn btn-warning" data-toggle="tooltip" data-placement="left" title="Tooltip en la izquierda">Botón Tooltip Izquierda</button>
+
+        <button class="btn btn-warning" data-toggle="tooltip" data-placement="right" title="Tooltip en la derecha">Botón Tooltip Derecha</button>
+  
  </div>
